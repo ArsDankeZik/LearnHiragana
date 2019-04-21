0d45f61e99ec4310caa2719e09c121b9b6066f6d
Richardo G <noctismaiestatem@gmail.com>
2019-04-22 00:10:56 +0200

    First Commit - Add Files

---
+++ JPGame/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace JPGame
+{
+    partial class JpLearn
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHiragana = new System.Windows.Forms.Label();
+            this.txtCheckSpell = new System.Windows.Forms.TextBox();
+            this.lblBad = new System.Windows.Forms.Label();
+            this.lblGood = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblHiragana
+            // 
+            this.lblHiragana.AutoSize = true;
+            this.lblHiragana.Font = new System.Drawing.Font("Segoe Print", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHiragana.Location = new System.Drawing.Point(161, 164);
+            this.lblHiragana.Name = "lblHiragana";
+            this.lblHiragana.Size = new System.Drawing.Size(0, 168);
+            this.lblHiragana.TabIndex = 0;
+            this.lblHiragana.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtCheckSpell
+            // 
+            this.txtCheckSpell.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCheckSpell.Location = new System.Drawing.Point(212, 449);
+            this.txtCheckSpell.Name = "txtCheckSpell";
+            this.txtCheckSpell.Size = new System.Drawing.Size(100, 50);
+            this.txtCheckSpell.TabIndex = 1;
+            this.txtCheckSpell.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.check);
+            // 
+            // lblBad
+            // 
+            this.lblBad.AutoSize = true;
+            this.lblBad.Location = new System.Drawing.Point(12, 9);
+            this.lblBad.Name = "lblBad";
+            this.lblBad.Size = new System.Drawing.Size(101, 42);
+            this.lblBad.TabIndex = 2;
+            this.lblBad.Text = "Bad: 0";
+            // 
+            // lblGood
+            // 
+            this.lblGood.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblGood.AutoSize = true;
+            this.lblGood.Location = new System.Drawing.Point(406, 9);
+            this.lblGood.Name = "lblGood";
+            this.lblGood.Size = new System.Drawing.Size(116, 42);
+            this.lblGood.TabIndex = 3;
+            this.lblGood.Text = "Good: 0";
+            // 
+            // JpLearn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 42F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(534, 511);
+            this.Controls.Add(this.lblGood);
+            this.Controls.Add(this.lblBad);
+            this.Controls.Add(this.txtCheckSpell);
+            this.Controls.Add(this.lblHiragana);
+            this.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 10, 8, 10);
+            this.MaximizeBox = false;
+            this.Name = "JpLearn";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JPLearn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHiragana;
+        private System.Windows.Forms.TextBox txtCheckSpell;
+        private System.Windows.Forms.Label lblBad;
+        private System.Windows.Forms.Label lblGood;
+    }
+}
+
---
+++ JPGame/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JPGame
+{
+    public partial class JpLearn : Form
+    {
+        private int good;
+        private int bad;
+        private int randomChoice;
+
+        private string[,] hiragana = new string[,]
+        {
+            {"あ", "a"},
+            {"か", "ka"},
+            {"さ", "sa"},
+            {"た", "ta"},
+            {"な", "na"},
+            {"は", "ha/wa"},
+            {"ま", "ma"},
+            {"や", "ya"},
+            {"ら", "ra"},
+            {"わ", "wa"},
+            {"が", "ga"},
+            {"ざ", "za"},
+            {"だ", "da"},
+            {"ば", "ba"},
+            {"ぱ", "pa"},
+
+            {"い", "i"},
+            {"き", "ki"},
+            {"し", "shi"},
+            {"ち", "chi"},
+            {"に", "ni"},
+            {"ひ", "hi"},
+            {"み", "mi"},
+            {"り", "ri"},
+            {"ぎ", "gi"},
+            {"じ", "ji"},
+            {"び", "bi"},
+            {"ぴ", "pi"},
+
+            {"う", "u"},
+            {"く", "ku"},
+            {"す", "su"},
+            {"つ", "tsu"},
+            {"ぬ", "nu"},
+            {"ふ", "fu/hu"},
+            {"む", "mu"},
+            {"ゆ", "yu"},
+            {"る", "ru"},
+            {"ぐ", "gu"},
+            {"ず", "zu"},
+            {"ぶ", "bu"},
+            {"ぷ", "pu"},
+
+            {"え", "e"},
+            {"け", "ke"},
+            {"せ", "se"},
+            {"て", "te"},
+            {"ね", "ne"},
+            {"へ", "he/e"},
+            {"め", "me"},
+            {"れ", "re"},
+            {"げ", "ge"},
+            {"ぜ", "ze"},
+            {"で", "de"},
+            {"べ", "be"},
+            {"ぺ", "pe"},
+
+            {"お", "o"},
+            {"こ", "ko"},
+            {"そ", "so"},
+            {"と", "to"},
+            {"の", "no"},
+            {"ほ", "ho"},
+            {"も", "mo"},
+            {"よ", "yo"},
+            {"ろ", "ro"},
+            {"を", "wo/o"},
+            {"ん", "n"},
+            {"ご", "go"},
+            {"ぞ", "zo"},
+            {"ど", "do"},
+            {"ぼ", "bo"},
+            {"ぽ", "po"},
+
+            {"や", "ya"},
+            {"きゃ", "kya"},
+            {"しゃ", "sha"},
+            {"ちゃ", "cha"},
+            {"にゃ", "nya"},
+            {"ひゃ", "hya"},
+            {"みゃ", "mya"},
+            {"りゃ", "rya"},
+            {"ぎゃ", "gya"},
+            {"じゃ", "ja"},
+            {"ぢゃ", "dja"},
+            {"びゃ", "bya"},
+            {"ぴゃ", "pya"},
+
+            {"ゆ", "yu"},
+            {"きゅ", "kyu"},
+            {"しゅ", "shu"},
+            {"ちゅ", "chu"},
+            {"にゅ", "nyu"},
+            {"ひゅ", "hyu"},
+            {"みゅ", "myu"},
+            {"りゅ", "ryu"},
+            {"ぎゅ", "gyu"},
+            {"じゅ", "ju"},
+            {"ぢゅ", "dju"},
+            {"びゅ", "byu"},
+            {"ぴゅ", "pyu"},
+
+            {"よ", "yo"},
+            {"きょ", "kyo"},
+            {"しょ", "sho"},
+            {"ちょ", "cho"},
+            {"にょ", "nyo"},
+            {"ひょ", "hyo"},
+            {"みょ", "myo"},
+            {"りょ", "ryo"},
+            {"ぎょ", "gyo"},
+            {"じょ", "gyo"},
+            {"ぢょ", "jo"},
+            {"びょ", "byo"},
+            {"ぴょ", "pyo"},
+
+            {"わ", "wa"},
+            {"くぁ", "kwa"},
+            {"ぐぁ", "gwa"}
+        };
+
+        public JpLearn()
+        {
+            InitializeComponent();
+            //MessageBox.Show($"{hiragana[5,0]} - {hiragana[5,1]}");
+            
+            assignValues();
+        }
+
+        private void assignValues()
+        {
+            randomChoice = rndSelect();
+            lblHiragana.Text = hiragana[randomChoice, 0];
+
+            lblBad.Text = $"Bad: {bad}";
+            lblGood.Text = $"Good: {good}";
+        }
+
+        private int rndSelect()
+        {
+            Random rnd = new Random();
+            return rnd.Next(hiragana.Length/2);
+        }
+
+        private void check(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)(Keys.Enter))
+            {
+                e.Handled = true;
+
+                if (hiragana[randomChoice, 1].Contains(txtCheckSpell.Text)) good++;
+                else bad++;
+
+                txtCheckSpell.Text = null;
+
+                assignValues();
+            }
+        }
+    }
+}
